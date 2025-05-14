e864b21b46c9f2854d92eb2e028cd2d18beb7fa0
Surendhar S <surendharsurendhar436@gmail.com>
2025-05-14 20:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const navToggle = document.getElementById('navToggle');
-const navLinks = document.getElementById('navLinks');
-const arrow = navToggle.querySelector('.arrow');
+const navToggle = document.getElementById("navToggle")
+const navLinks = document.getElementById("navLinks")
+const arrow = navToggle.querySelector(".arrow")
 
-navToggle.addEventListener('click', function () {
-  navLinks.classList.toggle('open');
-  arrow.style.transform = navLinks.classList.contains('open') ? 'rotate(180deg)' : 'rotate(0deg)';
-});
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("open")
+  arrow.style.transform = navLinks.classList.contains("open") ? "rotate(180deg)" : "rotate(0deg)"
+})
