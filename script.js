9adc5ed26b91e96b5e60b83d29a30c098103511f
Surendhar S <surendharsurendhar436@gmail.com>
2025-05-15 09:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const navToggle = document.getElementById("navToggle")
-const navLinks = document.getElementById("navLinks")
-const arrow = navToggle.querySelector(".arrow")
 
-navToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("open")
-  arrow.style.transform = navLinks.classList.contains("open") ? "rotate(180deg)" : "rotate(0deg)"
-})
